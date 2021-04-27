e04c0ba1193438f6044d875614440f26b5370cd5
RishabhAroraa <rishabharora.ns@gmail.com>
2021-04-27 11:59:54 +0530

    minor updates

--- lua/settings.lua
+++ lua/settings.lua
@@ -16,10 +16,7 @@ vim.wo.cursorline = true
 vim.cmd('let g:nvcode_termcolors=256')
 vim.cmd('syntax on')
 vim.cmd('colorscheme nvcode')
---vim.cmd('set termguicolors')
 vim.cmd('hi LineNr ctermbg=NONE guibg=NONE')
-
-
 
 vim.cmd('set ts=4')
 vim.cmd('set sw=4')
@@ -34,3 +31,23 @@ vim.cmd('let g:airline#extensions#tabline#enabled = 1')
 
 -- for c++ syntax highlighting
 vim.cmd('au FileType cpp let b:AutoPairs = AutoPairsDefine({"/*" : "*/"})')
+
+--for titlebars
+--vim.cmd("set title")
+--vim.cmd("set titleold")
+
+--coc config
+vim.cmd('set hidden')
+vim.cmd('set cmdheight=1')
+vim.cmd('set updatetime=300')
+vim.cmd('set shortmess+=c')
+vim.cmd('set signcolumn=yes')
+vim.cmd('hi signcolumn ctermbg=NONE guibg=NONE')
+
+--remove indent after access modifier in class
+vim.cmd('set cindent')
+vim.cmd('set cinoptions=g-1')
+
+--move between tabs
+vim.cmd('nnoremap <A-Left> :bprev<CR>');
+vim.cmd('nnoremap <A-Right> :bnext<CR>');
